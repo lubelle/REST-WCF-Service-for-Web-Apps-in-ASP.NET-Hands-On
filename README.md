e5ef3ef6bc24169b615ea6454ce8e243f5e4e33b
lubelle <belleliullh@gmail.com>
2019-09-08 14:37:16 -0400

    add wcf service (ajax-enabled)

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # REST-WCF-Service-for-Web-Apps-in-ASP.NET-Hands-On
 Implement WCF Service in C#<br />
+<ul>
+    <li>Create Service folder</li>
+    <li>Add new item -> Select WCF Service (Ajax-enabled)</li>
+    <li>System.Runtime.Serialization</li>
+    <li>System.ServiceModel</li>
+    <li>System.ServiceModel.Web</li>
+    <li>were added to the References</li>
+    <li>Markup of service file will not be updated if you change the namespace of your service</li>
+    <li>content of Web.config also changed once the service was added</li>
+    <li>Right click service -> View in Browser -> append operation name to service</li>
+</ul>
 Implement service calls with jQuery<br />
 Implement CRUD: Create, Read, Update, Delete<br />
 Implement additional methods: Filtering, sorting... <br />
--- Service/SuperHeroService.svc.cs
+++ Service/SuperHeroService.svc.cs
@@ -8,10 +8,18 @@ namespace SuperHeroDB.Service
     [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
     public class SuperHeroService
     {
+        // To use HTTP GET, add [WebGet] attribute. (Default ResponseFormat is WebMessageFormat.Json)
+        // To create an operation that returns XML,
+        //      add [WebGet(ResponseFormat = WebMessageFormat.Xml)],
+        //      add include the following line in the operation body:
+        //      WebOperationContext.Current.OutgoingResponse.ContentType = "text/xml";
+
         [OperationContract, WebGet(ResponseFormat = WebMessageFormat.Json)]
         public string DoWork()
         {
             return "This is the SuperHeroDB service!";
         }
+
+        // Add more operations here and mark them with [OperationContract]
     }
 }
